b4d05e1c73f5cad032d9a53d881d10936f63cb6e
ElyaDzhy <elmira.freshcode@gmail.com>
2023-04-06 11:15:07 +0300

    Document methods

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 'use strict'
+
+ const header = document.getElementById('header');
+
+ header.innerText = 'ewfwefew';
+ console.dir(header);
+
+ const arrLi = document.getElementsByTagName('li');
+
+ console.log(arrLi);
+
+ console.dir(arrLi[0]);
+
+ const cnArr = document.getElementsByClassName('cn');
+ console.log(cnArr)
