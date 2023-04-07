6bdc20bdf56543a2d3a7e650c5df4b4fa81772d2
ElyaDzhy <elmira.freshcode@gmail.com>
2023-04-07 10:21:12 +0300

    Add event bubbling and immersion example

--- src/index.js
+++ src/index.js
@@ -2,17 +2,11 @@
 
 //Events
 
-const btn = document.querySelectorAll('.btn');
-
-function clickHandler(){
-    
-    this.style.backgroundColor = 'red';
-
-    btn.forEach((button)=> button.removeEventListener('click', clickHandler));
-
-}
-
-btn.forEach((button) => button.addEventListener('click', clickHandler));
+const btn = document.querySelector('button');
+const section = document.querySelector('section');
+const div = document.querySelector('div');
 
 
-
+btn.addEventListener('click', () => console.log(console.log('btn')), true);
+section.addEventListener('click', () => console.log(console.log('section')), true);
+div.addEventListener('click', () => console.log(console.log('div')), true);
