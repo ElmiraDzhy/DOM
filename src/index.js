eee5d669f9c05fcdefbd6b53e58f2543f5358834
ElyaDzhy <elmira.freshcode@gmail.com>
2023-04-07 11:31:25 +0300

    Add preventDefault() example

--- src/index.js
+++ src/index.js
@@ -2,5 +2,24 @@
 
 //Events
 
-const btn = document.querySelectorAll('.btn');
-btn.forEach((button) => button.addEventListener('click', (e)=> e.target.remove()));
+const div = document.querySelector('div');
+const btn = document.querySelector('button');
+const form = document.querySelector('form');
+
+
+div.addEventListener('click', (e)=> {
+
+    console.log(e.target);
+    if(e.target === e.currentTarget){
+        e.target.children[0].style.color = 'red';
+    }
+}, true);
+
+btn.addEventListener('click', (e)=> console.log(e.target));
+
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    console.log(e);
+
+})
