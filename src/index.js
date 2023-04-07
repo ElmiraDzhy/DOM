11641e99529a08696af194ef0a3bb2c0c870dbd0
ElyaDzhy <elmira.freshcode@gmail.com>
2023-04-07 10:52:22 +0300

    Add task #3 resolve

--- src/index.js
+++ src/index.js
@@ -7,6 +7,15 @@ const section = document.querySelector('section');
 const div = document.querySelector('div');
 
 
-btn.addEventListener('click', () => console.log(console.log('btn')), true);
-section.addEventListener('click', () => console.log(console.log('section')), true);
-div.addEventListener('click', () => console.log(console.log('div')), true);
+btn.addEventListener('click', (e) => {
+    console.log(console.log(e));
+    e.target.style.backgroundColor = 'blue';
+}, true);
+section.addEventListener('click', (e) => console.log(console.log(e)), true);
+div.addEventListener('click', (e) => console.log(console.log(e)), true);
+
+const event = new Event('click');
+console.log(event);
+
+const p = document.querySelector('p');
+p.addEventListener('click', (e)=> {e.target.style.color = 'yellow'});
