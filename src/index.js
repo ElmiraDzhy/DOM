09bc60be253b2d96404e4f0fe2c6163cb39cf898
ElyaDzhy <elmira.freshcode@gmail.com>
2023-04-07 11:05:16 +0300

    Add 5 btn task resolve

--- src/index.js
+++ src/index.js
@@ -2,20 +2,5 @@
 
 //Events
 
-const btn = document.querySelector('button');
-const section = document.querySelector('section');
-const div = document.querySelector('div');
-
-
-btn.addEventListener('click', (e) => {
-    console.log(console.log(e));
-    e.target.style.backgroundColor = 'blue';
-}, true);
-section.addEventListener('click', (e) => console.log(console.log(e)), true);
-div.addEventListener('click', (e) => console.log(console.log(e)), true);
-
-const event = new Event('click');
-console.log(event);
-
-const p = document.querySelector('p');
-p.addEventListener('click', (e)=> {e.target.style.color = 'yellow'});
+const btn = document.querySelectorAll('.btn');
+btn.forEach((button) => button.addEventListener('click', (e)=> e.target.remove()));
