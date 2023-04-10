d2fc547bc2a62e1d9554fa2bd8ee89d3b9ec80aa
ElyaDzhy <elmira.freshcode@gmail.com>
2023-04-10 09:01:55 +0300

    Add menu

--- src/index.js
+++ src/index.js
@@ -1,39 +1,10 @@
 'use strict'
 
-//Events
+const btn = document.querySelector('.btn');
+const menu = document.querySelector('.menu');
 
-const parent = document.querySelector('section');
-const div = document.querySelector('.score');
-const btn = document.querySelector('button');
+const closeBtn = document.querySelector('.close-btn');
 
+btn.addEventListener('click', ()=> menu.style.left = '0px');
 
-let counter = 0;
-
-
-function increment(){
-    counter++;
-    updateView();
-
-}
-function decrement(){
-    counter--;
-    updateView();
-
-}
-
-function updateView(){
-    div.textContent = counter;
-
-}
-
-
-
-
-
-parent.addEventListener('click', (e)=>{
-    if(e.currentTarget === e.target){
-        increment();
-    }else{
-        decrement();
-    }
-})
+closeBtn.addEventListener('click', ()=> menu.style.left = '-300px')
