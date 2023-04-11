8a8691fdf00c3b21f545f16b0991de7f4efb9830
ElyaDzhy <elmira.freshcode@gmail.com>
2023-04-11 11:01:35 +0300

    Add task#4 resolve

--- src/index.js
+++ src/index.js
@@ -2,34 +2,14 @@
 
 //Unit 49
 
-const input = document.querySelector('[name="passInput"]');
-const passValidation = document.querySelector('.pass-validation');
+const btn = document.querySelector('button');
 
-input.addEventListener('change', ({target: {value: {length}}}) => {
+btn.addEventListener('click', createSquare);
+
+function createSquare(){
+    const div = document.createElement('div');
+    div.classList.add('circle');
+    document.body.append(div);
 
 
-    clearClasses();
-
-    if(length < 5){
-        showMsg('Weak', 'show-weak');
-    } 
-
-    if(length >= 5 && input.value.length <= 7){
-        showMsg('Middle', 'show-middle');
-    }
-    
-    if(length >= 8) {
-        showMsg('Strong', 'show-good');
-    }
-});
-
-function showMsg(type, cls){
-    passValidation.textContent = type;
-    passValidation.classList.add(cls);
-}
-
-function clearClasses(){
-    passValidation.classList.remove('show-weak');
-    passValidation.classList.remove('show-middle');
-    passValidation.classList.remove('show-good');
 }
