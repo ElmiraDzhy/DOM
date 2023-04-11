8349edbf387c118835a94a8dbfa9dbcd2296b6fe
ElyaDzhy <elmira.freshcode@gmail.com>
2023-04-11 11:16:04 +0300

    Random circles

--- src/index.js
+++ src/index.js
@@ -7,9 +7,19 @@ const btn = document.querySelector('button');
 btn.addEventListener('click', createSquare);
 
 function createSquare(){
+
     const div = document.createElement('div');
     div.classList.add('circle');
+    const parentWidth = document.body.offsetWidth;
+    const parentHeight = document.body.offsetHeight;
+
+    div.style.top = `${getRandomCoordinate(0, parentHeight)}px`;
+    div.style.left = `${getRandomCoordinate(0, parentWidth)}px`;
+
     document.body.append(div);
+}
 
 
+function getRandomCoordinate(min, max){
+    return Math.random() * (max - min) + min;
 }
