6e349ec1c1f3d284a26915fa31a6067a9563860c
ElyaDzhy <elmira.freshcode@gmail.com>
2023-04-11 09:54:54 +0300

    Add task#2 resolve

--- src/index.js
+++ src/index.js
@@ -7,13 +7,17 @@ const errorContainer = document.querySelector('.error')
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    
+    errorContainer.textContent = '';
+    errorContainer.classList.remove('swoh-error');
     const {emailInput, passInput} = e.target;
 
     const email = emailInput.value;
     const pass = passInput.value;
     if(!email.includes('@')){
         errorContainer.textContent = 'Email is not valid';
+        errorContainer.classList.add('show-error');
+    } else if(pass.toLowerCase().includes('qwerty') || pass.length <= 5){
+        errorContainer.textContent = 'Pass is not valid';
         errorContainer.classList.add('show-error');
     }
 });
